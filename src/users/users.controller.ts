539efa3b817686d1e184d685cf6c88b60972bdd8
Mx444 <mybbtester@gmail.com>
2024-09-20 12:31:46 +0200

    feat: mapped types

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,17 +17,17 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 @Controller('users')
 export class UsersController {
-  @Get('/:id')
+  @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(id + ' ' + typeof id);
-    console.log(limit + ' ' + typeof limit);
-    console.log(page + ' ' + typeof page);
+    console.log(getUsersParamDto);
     return 'Yuo sent a get request to users endpoint';
   }
 
@@ -35,5 +35,10 @@ export class UsersController {
   public createUsers(@Body() createUserDto: CreateUserDto) {
     console.log(createUserDto);
     return 'Yuo sent a post request to users endpoint';
+  }
+
+  @Patch()
+  public patchUSers(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }

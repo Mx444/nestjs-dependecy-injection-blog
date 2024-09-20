6166211b69bca7a6aecbf644c1a3592f6f2cd71e
Mx444 <mybbtester@gmail.com>
2024-09-20 13:35:53 +0200

    feat: DI users into posts

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly usersService: UsersService) {}
+  public findAll(userId: string) {
+    const user = this.usersService.findOneById(userId);
+    console.log(userId);
+    return [
+      {
+        userId: user,
+        title: 'Post 1',
+        content: 'Post 1 content',
+      },
+      {
+        title: 'Post 2',
+        content: 'Post 2 content',
+      },
+    ];
+  }
+}

3f1df51c179cf5cea8c29c0fe16a142d80899763
Mx444 <mybbtester@gmail.com>
2024-09-20 13:43:53 +0200

    feat: new module auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './providers/users.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],

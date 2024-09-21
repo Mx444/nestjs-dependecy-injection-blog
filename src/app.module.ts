b0059676b3bf6136b3da85a1efec05cac81b73b8
Mx444 <mybbtester@gmail.com>
2024-09-21 12:57:55 +0200

    feat: entity tags

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import 'dotenv/config';
 import { User } from './users/user.entity';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { User } from './users/user.entity';
         logging: process.env.DB_LOGGING === 'true',
       }),
     }),
+    TagsModule,
   ],
 
   controllers: [AppController],

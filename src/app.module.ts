1a0729bd767f3917a86300879d7088a503740b3c
Mx444 <mybbtester@gmail.com>
2024-09-21 11:00:24 +0200

    feat: config typeorm postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,32 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: process.env.DB_TYPE as any,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [User],
+        synchronize: true,
+        logging: process.env.DB_LOGGING === 'true',
+      }),
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
